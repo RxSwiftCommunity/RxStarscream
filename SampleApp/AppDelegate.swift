0b354fc2c34204d20ccc3ba14c79121d54d3003c
Guy Kahlon <guykahlon@gmail.com>
2016-06-01 18:43:56 +0300

    Add Sample project.

---
+++ SampleApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SampleApp
+//
+//  Created by Guy Reporty on 01/06/2016.
+//  Copyright © 2016 Guy Kahlon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
+
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Guy Reporty on 01/06/2016.
+//  Copyright © 2016 Guy Kahlon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+    
+  }
+}
+
