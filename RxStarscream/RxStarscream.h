822f91a795c7108bfbed76be63002fe0246ec468
Guy Kahlon <guykahlon@gmail.com>
2016-06-01 17:46:22 +0300

    Improvement: Change for inheritance from WebSocket to extension. Add Carthage support.

---
+++ RxStarscream/RxStarscream.h
@@ -0,0 +1,19 @@
+//
+//  RxStarscream.h
+//  RxStarscream
+//
+//  Created by Guy Reporty on 01/06/2016.
+//  Copyright © 2016 Guy Kahlon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxStarscream.
+FOUNDATION_EXPORT double RxStarscreamVersionNumber;
+
+//! Project version string for RxStarscream.
+FOUNDATION_EXPORT const unsigned char RxStarscreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxStarscream/PublicHeader.h>
+
+
--- Source/RxStarscream.swift
+++ Source/RxStarscream.swift
@@ -1,6 +1,5 @@
 //
-//  Created by Jesse Squires
-//  Guy Kahlon
+//  Created by Guy Kahlon
 //
 
 import Foundation
@@ -15,87 +14,161 @@ public enum WebSocketEvent {
   case Pong
 }
 
-public class RxWebSocket: WebSocket {
+extension WebSocket {
   
-  private let subject = PublishSubject<WebSocketEvent>()
-  private var forwardDelegate: WebSocketDelegate?
-  private var forwardPongDelegate: WebSocketPongDelegate?
-  
-  public override weak var delegate: WebSocketDelegate? {
-    didSet {
-      if delegate === self {
-        return
-      }
-      forwardDelegate = delegate
-      delegate = self
-    }
+  static public func rx_webSocket(url: NSURL, protocols: [String]? = nil) -> Observable<WebSocket> {
+    
+    return Observable.create { observer in
+      
+      let webSocket = WebSocket(url: url, protocols: protocols)
+      
+      observer.onNext(webSocket)
+      
+      return NopDisposable.instance
+      
+      }.shareReplayLatestWhileConnected()
   }
+}
+
+var connectKey: UInt8 = 0
+var disconnectKey: UInt8 = 0
+var textKey: UInt8  = 0
+var dataKey: UInt8  = 0
+var pongKey: UInt8  = 0
+
+extension WebSocket {
   
-  public override weak var pongDelegate: WebSocketPongDelegate? {
-    didSet {
-      if pongDelegate === self {
-        return
-      }
-      forwardPongDelegate = pongDelegate
-      pongDelegate = self
-    }
-  }
-  
-  public private(set) lazy var rx_response: Observable<WebSocketEvent> = {
-    return self.subject
-  }()
-  
-  public private(set) lazy var rx_text: Observable<String> = {
-    return self.subject.filter { response in
-      switch response {
-      case .Message(_):
-        return true
-      default:
-        return false
-      }
-      }.map { response in
-        switch response {
-        case .Message(let message):
-          return message
-        default:
-          return String()
+  public var rx_connect: Observable<Void> {
+    
+    return memoize(&connectKey) {
+      
+      Observable.create { [weak self] observer in
+        
+        guard let webSocket = self else {
+          observer.on(.Completed)
+          return NopDisposable.instance
         }
+        
+        webSocket.onConnect = {
+          observer.on(.Next())
+        }
+        
+        return NopDisposable.instance
+        }
+        .shareReplayLatestWhileConnected()
     }
-  }()
+  }
   
-  public override func connect() {
-    super.connect()
-    delegate = self
-    pongDelegate = self
+  public var rx_disconnect: Observable<NSError?> {
+    
+    return memoize(&disconnectKey) {
+      
+      Observable.create { [weak self] observer in
+        
+        guard let webSocket = self else {
+          observer.on(.Completed)
+          return NopDisposable.instance
+        }
+        
+        webSocket.onDisconnect = { error in
+          observer.on(.Next(error))
+        }
+        
+        return NopDisposable.instance
+        }
+        .shareReplayLatestWhileConnected()
+    }
+  }
+  
+  public var rx_text: Observable<String> {
+    
+    return memoize(&textKey) {
+      
+      Observable.create { [weak self] observer in
+        
+        guard let webSocket = self else {
+          observer.on(.Completed)
+          return NopDisposable.instance
+        }
+        
+        webSocket.onText = { text in
+          observer.on(.Next(text))
+        }
+        
+        return NopDisposable.instance
+        }
+        .shareReplayLatestWhileConnected()
+    }
+  }
+  
+  public var rx_data: Observable<NSData> {
+    
+    return memoize(&dataKey) {
+      
+      Observable.create { [weak self] observer in
+        
+        guard let webSocket = self else {
+          observer.on(.Completed)
+          return NopDisposable.instance
+        }
+        
+        
+        webSocket.onData = { data in
+          observer.on(.Next(data))
+        }
+        
+        return NopDisposable.instance
+        }
+        .shareReplayLatestWhileConnected()
+    }
+  }
+  
+  public var rx_pong: Observable<Void> {
+    
+    return memoize(&pongKey) {
+      
+      Observable.create { [weak self] observer in
+        
+        guard let webSocket = self else {
+          observer.on(.Completed)
+          return NopDisposable.instance
+        }
+        
+        
+        webSocket.onPong = {
+          observer.on(.Next())
+        }
+        
+        return NopDisposable.instance
+        }
+        .shareReplayLatestWhileConnected()
+    }
+  }
+  
+  public var rx_response: Observable<WebSocketEvent> {
+    
+    let connect = rx_connect.map { WebSocketEvent.Connected }
+    let disconnect = rx_disconnect.map { WebSocketEvent.Disconnected($0) }
+    let text = rx_text.map { WebSocketEvent.Message($0) }
+    let data = rx_data.map { WebSocketEvent.Data($0) }
+    let pong = rx_pong.map { WebSocketEvent.Pong }
+    
+    return Observable.of(connect, disconnect, text, data, pong).merge()
   }
 }
 
-extension RxWebSocket: WebSocketPongDelegate {
-  public func websocketDidReceivePong(socket: WebSocket) {
-    subject.on(.Next(WebSocketEvent.Pong))
-  }
-}
-
-extension RxWebSocket: WebSocketDelegate {
+extension WebSocket {
   
-  public func websocketDidConnect(socket: WebSocket) {
-    subject.on(.Next(WebSocketEvent.Connected))
-    forwardDelegate?.websocketDidConnect(socket)
-  }
-  
-  public func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
-    subject.on(.Next(WebSocketEvent.Disconnected(error)))
-    forwardDelegate?.websocketDidDisconnect(socket, error: error)
-    socket.delegate = nil
-  }
-  
-  public func websocketDidReceiveMessage(socket: WebSocket, text: String) {
-    subject.on(.Next(WebSocketEvent.Message(text)))
-    forwardDelegate?.websocketDidReceiveMessage(socket, text: text)
-  }
-  
-  public func websocketDidReceiveData(socket: WebSocket, data: NSData) {
-    subject.on(.Next(WebSocketEvent.Data(data)))
-    forwardDelegate?.websocketDidReceiveData(socket, data: data)
+  func memoize<D>(key: UnsafePointer<Void>, createLazily: () -> Observable<D>) -> Observable<D> {
+    objc_sync_enter(self); defer { objc_sync_exit(self) }
+    
+    if let sequence = objc_getAssociatedObject(self, key) as? Observable<D> {
+      return sequence
+    }
+    
+    let sequence = createLazily()
+    objc_setAssociatedObject(self, key, sequence, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    
+    return sequence
   }
 }
