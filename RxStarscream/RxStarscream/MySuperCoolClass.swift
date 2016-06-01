f5e7a03eb8df85199a5247e81f6ac97b9a9c8cca
Guy Kahlon <guykahlon@gmail.com>
2016-06-01 16:24:00 +0300

    update

---
+++ RxStarscream/RxStarscream/MySuperCoolClass.swift
@@ -0,0 +1,21 @@
+//
+//  MySuperCoolClass.swift
+//  RxStarscream
+//
+//  Created by Guy Reporty on 01/06/2016.
+//  Copyright © 2016 Guy Kahlon. All rights reserved.
+//
+
+import Foundation
+public class MySuperCoolClass {
+
+  
+  public func doTheThing()  {
+    print("asdasdasdasd")
+  }
+  
+  public func doAnotherThing() {
+    print("doAnotherThing")
+    // this func not visibile from project linking this framework
+  }
+}
