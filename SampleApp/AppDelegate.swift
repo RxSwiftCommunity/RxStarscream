26d5faa1a8e8960e6b805e401ffa5dd581c99666
Cezary Kopacz <salvation.sv@gmail.com>
2017-03-25 12:12:24 -0400

    Fixed not working pong delegate, added send pong button, fixed formatting

--- SampleApp/AppDelegate.swift
+++ SampleApp/AppDelegate.swift
@@ -8,9 +8,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-    
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
     return true
   }
 }
-
--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -7,62 +7,74 @@ import RxSwift
 import Starscream
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var inputTextField: UITextField!
-  @IBOutlet weak var sendButton: UIButton!
-  @IBOutlet weak var logTextView: UITextView!
-  
-  let disposeBag = DisposeBag()
-  let socket = WebSocket(url: URL(string: "wss://echo.websocket.org")!)
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    socket.rx.response
-      .subscribe(onNext: { [weak self] (response: WebSocketEvent) in
-        
-        guard let `self` = self else {
-          return
-        }
-        
-        switch response {
-        case .connected:
-          self.logTextView.text = self.logTextView.text + "Connected\n"
-        case .disconnected(let error):
-          self.logTextView.text = self.logTextView.text + "Disconnected with optional error: \(error) \n "
-        case .message(let msg):
-          self.logTextView.text = self.logTextView.text + "RESPONSE (Message): \(msg) \n"
-        case .data(let data):
-          self.logTextView.text = self.logTextView.text + "RESPONSE (Data): \(data) \n"
-        case .pong:
-          self.logTextView.text = self.logTextView.text + "RESPONSE (Pong)"
-        }
-      }).addDisposableTo(disposeBag)
-    
-    socket.connect()
-  }
 
-  @IBAction func sendButtonAction(sender: UIButton) {
-    if let text = inputTextField.text, !text.isEmpty {
-      sendMessage(message: text)
+    @IBOutlet fileprivate weak var inputTextField: UITextField!
+    @IBOutlet fileprivate weak var sendButton: UIButton!
+    @IBOutlet fileprivate weak var logTextView: UITextView!
+    @IBOutlet fileprivate weak var pongButton: UIBarButtonItem!
+
+    private let disposeBag = DisposeBag()
+    private let socket = WebSocket(url: URL(string: "wss://echo.websocket.org")!)
+    private let writeSubject = PublishSubject<String>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sendButton.rx.tap
+                .subscribe(onNext: { [unowned self] in
+                    guard let text = self.inputTextField.text, !text.isEmpty else {
+                        return
+                    }
+                    self.sendMessage(message: text)
+                }).disposed(by: disposeBag)
+
+        pongButton.rx.tap.subscribe(onNext: { [unowned self] in
+                    self.socket.write(ping: Data())
+                    self.writeSubject.onNext("PING")
+                }).disposed(by: disposeBag)
+        
+        let responseString = socket.rx.response
+            .map { response -> String in
+                switch response {
+                case .connected:
+                    return "Connected\n"
+                case .disconnected(let error):
+                    return "Disconnected with optional error: \(error) \n "
+                case .message(let msg):
+                    return "RESPONSE (Message): \(msg) \n"
+                case .data(let data):
+                    return  "RESPONSE (Data): \(data) \n"
+                case .pong:
+                    return "RESPONSE (Pong)"
+                }
+        }
+        
+        Observable.merge([responseString, writeSubject.asObservable()])
+            .scan([]) { lastMsg, newMsg -> Array<String> in
+                return Array(lastMsg + [newMsg])
+            }.map { $0.joined(separator: "\n")
+            }.asDriver(onErrorJustReturn: "")
+            .drive(logTextView.rx.text)
+            .disposed(by: disposeBag)
+        
+        socket.connect()
     }
-  }
-  
-  fileprivate func sendMessage(message: String) {
-    socket.write(string: message)
-    logTextView.text = logTextView.text + "SENT: \(message) \n"
-    inputTextField.text = nil
-    inputTextField.resignFirstResponder()
-  }
+
+    fileprivate func sendMessage(message: String) {
+        socket.write(string: message)
+        writeSubject.onNext("SENT: \(message)")
+        inputTextField.text = nil
+        inputTextField.resignFirstResponder()
+    }
 }
 
 extension ViewController: UITextFieldDelegate {
-  
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    if let text = textField.text, !text.isEmpty {
-      sendMessage(message: text)
-      return true
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text, !text.isEmpty {
+            sendMessage(message: text)
+            return true
+        }
+        return false
     }
-    return false
-  }
 }
